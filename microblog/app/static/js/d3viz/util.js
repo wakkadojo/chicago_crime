8038742687c058d7d76048110f347f7d8407dc65
Jake Ellowitz <jake.ellowitz@gmail.com>
2017-11-05 19:19:21 -0500

    +utility file

---
+++ microblog/app/static/js/d3viz/util.js
@@ -0,0 +1,11 @@
+
+// For every column in the csv, create a timeseries of the value in the column
+// This is similar to nest but allows time preprocessing (since nest will convert the time to a string)
+function csv_timeseries_col_split(csv_data, cols_to_plot) {
+    return Object.keys(csv_data[0]).filter(key => cols_to_plot.has(key)).map(function(id) {
+        return {
+            id     : id,
+            values : csv_data.map(function(d) { return { time : d.time, value : d[id] }; })
+        };
+    });
+}
