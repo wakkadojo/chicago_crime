4d8dba57ec6f305215bf2b5f66e2de84b25df63b
Jake Ellowitz <jake.ellowitz@gmail.com>
2017-11-16 12:03:36 -0500

    Changing titles and background

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -5,8 +5,18 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template(
+        "index.html", 
+        title = "Jake Ellowitz",
+        subtitle = "A man on no mission whatsoever",
+        background_img_name = "home-bg.jpg"
+    )
 
 @app.route('/chicago')
 def chicago():
-    return render_template("chicago.html")
+    return render_template(
+        "chicago.html", 
+        title = "Crime In Chicago",
+        subtitle = "A Portrait of Crime Inequality",
+        background_img_name = "chicago_skyline_style_xfer.jpg"
+    )
