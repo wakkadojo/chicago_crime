e4cc56c28f135c47ae0c118f7e462897b66bcae4
Jake Ellowitz <jake.ellowitz@gmail.com>
2018-02-05 16:54:46 -0800

    Some cleanup; make a contact form

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,20 +1,11 @@
-from flask import render_template
+from flask import render_template, Flask, flash, request
+from flask_sendgrid import SendGrid
 from app import app
-
-
-#@app.route('/')
-#@app.route('/index')
-#def index():
-#    return render_template(
-#        "index.html", 
-#        title = "Jake Ellowitz",
-#        subtitle = "A Man on no Mission Whatsoever",
-#        background_img_name = "home-bg.jpg"
-#    )
+import os
+import json
 
 @app.route('/')
 @app.route('/index')
-#@app.route('/chicago')
 def chicago():
     return render_template(
         "chicago.html", 
@@ -23,11 +14,41 @@ def chicago():
         background_img_name = "chicago_skyline_style_xfer.jpg"
     )
 
-#@app.route('/contact')
-#def contact():
-#    return render_template(
-#        "contact.html",
-#        title = "Contact Me",
-#        subtitle = "And I will solve all of your problems",
-#        background_img_name = "home-bg.jpg"
-#    )
+def send_email(name, email, message):
+    msg = 'Name: ' + name + '\n\n'
+    msg += 'Email: ' + email + '\n\n'
+    msg += message
+
+    if os.path.exists('app/static/mail/sendgrid_api.json'):
+
+        with open('app/static/mail/sendgrid_api.json') as f:
+            api_key = json.loads(f.read())['key']
+
+        app_tmp = Flask(__name__)
+        app_tmp.config['SENDGRID_API_KEY'] = api_key
+        app_tmp.config['SENDGRID_DEFAULT_FROM'] = 'chicago@crimeinequality.com'
+        sendgrid = SendGrid(app_tmp)
+        sendgrid.send_email(
+            to_email=[{'email': 'jake.ellowitz@gmail.com'}],
+            subject='Chicago Crime Inequality',
+            text=msg
+        )
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+
+    if request.method == 'POST':
+        send_email(request.form['name'], request.form['email'], request.form['message'])
+        return render_template(
+            "contactSuccess.html",
+            title = "Contact Me",
+            subtitle = "Share your thoughts!",
+            background_img_name = "chicago_skyline_style_xfer.jpg"
+        )
+    else:
+        return render_template(
+            "contact.html",
+            title = "Contact Me",
+            subtitle = "Share your thoughts!",
+            background_img_name = "chicago_skyline_style_xfer.jpg"
+        )
--- microblog/run.py
+++ microblog/run.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 from app import app
+from random import randint
 
 if __name__ == '__main__':
+
+    app.secret_key = str(randint(0, 1000000000))
     app.run(debug = True)
